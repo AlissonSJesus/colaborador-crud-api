6fdf4394e63308dfe84ef741ca0d12919634eb4f
Alisson Santos de Jesus <alisson.jesus@salvador.ba.gov.br>
2022-09-01 13:32:03 -0300

    Alteração do package.json

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # colaborador-crud-api
 Aplicação simulando CRUD com API
+
+## Links e recursos
+
+http://nandovieira.com.br/usando-uuid-no-postgresql-com-activerecord
+
+http://www.postgresqltutorial.com/postgresql-date/
---
+++ script-database/script-empregado.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employee (
+	id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+	nome VARCHAR(255) NOT NULL,
+	cargo VARCHAR(255) NOT NULL,
+	salary NUMERIC(5,2),
+	data_nascimento DATE NOT NULL,
+	matricula INTEGER NOT NULL
+)
