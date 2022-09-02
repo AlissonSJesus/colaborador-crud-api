b40f4dd995564ed0150f389e7c0544f69fac77e7
Alisson Santos de Jesus <alisson.jesus@salvador.ba.gov.br>
2022-09-02 15:38:09 -0300

    Criando a rota POST - Employee

--- sql/script-empregado.sql
+++ sql/script-empregado.sql
@@ -2,7 +2,7 @@ CREATE TABLE employee (
 	id uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
 	nome VARCHAR(255) NOT NULL,
 	cargo VARCHAR(255) NOT NULL,
-	salary NUMERIC(5,2),
+	salary NUMERIC(7,2),
 	data_nascimento DATE NOT NULL,
 	matricula INTEGER NOT NULL
 )
